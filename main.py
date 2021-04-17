c88cb6789bd5c2ba8505a316dbab9171e94e0ea8
lironne <lironkurz@gmail.com>
2021-04-17 14:21:53 -0700

    created load data

---
+++ main.py
@@ -0,0 +1,8 @@
+import util
+
+
+def main():
+    util.load_data()
+
+if __name__ == "__main__":
+    main()
---
+++ util.py
@@ -0,0 +1,37 @@
+import glob
+import os
+import numpy as np
+import pandas as pd
+
+dirname = os.path.dirname(__file__)
+data_path = os.path.join(dirname, 'data/*.csv')
+
+
+def load_data():
+
+    data = {}
+
+    for data_set in glob.glob(data_path):
+
+        path, name = os.path.split(data_set)
+        name = os.path.splitext(name)[0]
+        name = name.split('_')
+
+        water_type = ''
+        water_name = ''
+
+        if(len(name) == 2):
+            water_type = name[0]
+            water_name = name[1]
+        else:
+            water_type = name[1]
+            water_name = name[2]
+
+        if(water_type not in data):
+            data[water_type] = {}
+        
+        data[water_type][water_name] = pd.read_csv(data_set)
+    
+    return data 
+    
+
