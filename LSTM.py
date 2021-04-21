8b0326882ae2da388b078a888a04b4c6750079e5
lironne <lironkurz@gmail.com>
2021-04-21 11:54:06 -0700

    created LSTM

--- LSTM.py
+++ LSTM.py
@@ -1,3 +1,21 @@
+import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+
+def split_data(data):
+
+    # split into train and test sets
+    values = data.values
+    n_train_days = 365 * 24
+    train = values[:n_train_days, :]
+    test = values[n_train_days:, :]
+    # split into input and outputs
+    train_X, train_y = train[:, :-1], train[:, -1]
+    test_X, test_y = test[:, :-1], test[:, -1]
+    # reshape input to be 3D [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+    return (train_X,train_y,test_X,test_y)
